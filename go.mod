bea472baf4662452266ee49c8da84cf1ea6f88a8
Olivier Ory <mail@normegil.be>
2020-04-27 08:29:52 +0200

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/normegil/godatabaseversioner
+
+go 1.13
---
+++ package.go
@@ -0,0 +1,2 @@
+// Go database versioner will help you to manage your database schema versions.
+package godatabaseversioner
